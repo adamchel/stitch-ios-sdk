051473e9a01004b54c818e667238f6324d1c4874
Adam Chelminski <adam.chelminski@mongodb.com>
2018-12-19 16:14:35 -0500

    do some wacky stuff and add the dummy project

---
+++ Dummy/Dummy/Dummy.h
@@ -0,0 +1,19 @@
+//
+//  Dummy.h
+//  Dummy
+//
+//  Created by Adam Chelminski on 12/19/18.
+//  Copyright © 2018 MongoDB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Dummy.
+FOUNDATION_EXPORT double DummyVersionNumber;
+
+//! Project version string for Dummy.
+FOUNDATION_EXPORT const unsigned char DummyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Dummy/PublicHeader.h>
+
+
